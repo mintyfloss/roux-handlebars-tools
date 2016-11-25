ae9a58bb7dec780580aa0d702f90bce8e5fc3982
Lon Ingram <lawnsea@gmail.com>
2016-11-25 12:20:17 -0600

    Set up TravisCI
    Fix #2

--- test/index.js
+++ test/index.js
@@ -58,7 +58,7 @@ tap.test('getPartialDependencies', function (t) {
 				],
 				function (arg) {
 					t.throws(function () {
-						resolve('', arg);
+						getPartialDependencies('', arg);
 					}, 'must be an object, not ' + arg);
 				});
 
@@ -844,8 +844,9 @@ tap.test('registerPartial', (t) => {
 	t.test('registers partials', (t) => {
 		t.autoend();
 
-		t.afterEach(() => {
+		t.afterEach((done) => {
 			mockfs.restore();
+			done();
 		});
 
 		t.test('with no dependencies', (t) => {
@@ -1046,8 +1047,9 @@ tap.test('registerPantry', (t) => {
 	t.test('registers ingredients', (t) => {
 		t.autoend();
 
-		t.afterEach(() => {
+		t.afterEach((done) => {
 			mockfs.restore();
+			done();
 		});
 
 		t.test('in the pantry when registerOptions are specified', (t) => {
